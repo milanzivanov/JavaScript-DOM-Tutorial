ae75a8a603c71dec1859e630958b2d3819ad39d0
milan.zivanov <milan.zivanov@gmail.com>
2018-09-06 12:30:06 +0200

    Events

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+let h2 = document.querySelector("#book-list h2");
 
-const bookList = document.querySelector("#book-list");
-console.log("The parent node: ", bookList.parentNode);
-console.log("The parent node: ", bookList.parentElement.parentElement);
-console.log(bookList.childNodes);
-
-console.log("book-list next sibling: ", bookList.nextSibling);
-console.log("book-list next sibling: ", bookList.nextElementSibling);
-
-console.log("book-list previous sibling is: ", bookList.previousSibling);
-console.log("book-list element sibling is: ", bookList.previousElementSibling);
+h2.addEventListener("click", function(e) {
+    console.log(e.target);
+    console.log(e);
+});
 
 
-bookList.previousElementSibling.querySelector("p").innerHTML += "<br /> Too cool for everyone else"
+
+const listItems = document.querySelectorAll('#book-list ul li');
+Array.from(listItems).forEach(function(item){
+  item.addEventListener('click', (e) => {
+
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+
+  });
+});
+
+
+// prevent default behaviour
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+});
