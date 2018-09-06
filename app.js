577ab7fcda8dead94dbf2acc39729342704892e9
milan.zivanov <milan.zivanov@gmail.com>
2018-09-06 11:28:27 +0200

    changing text and html content, Nodes

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-const banner = document.getElementById("page-banner");
-const bookList = document.getElementById("book-list");
-
-let titles = document.getElementsByClassName("title");
-let lis = document.getElementsByTagName("li");
-
-
-// chaking is something a array
-console.log(Array.isArray(titles));
-
-// transforming to array
-console.log(Array.isArray(Array.from(titles)));
-
-
-Array.from(titles).forEach((item) => {
-    console.log(item);
-});
-
-for(i=0; i < titles.length; i++) {
-    console.log(titles[i]);
-}
-
 
 const wmf = document.querySelector("#book-list li:nth-child(2) .name")
 // console.log(wmf);
 var books = document.querySelectorAll("#book-list li .name");
 // console.log(books);
 
-Array.from(books).forEach(book => console.log(book));
+books.forEach(book => {
+    book.textContent += ' (book title)';
+});
+
+const bookList2 = document.querySelector("#book-list");
+// bookList2.innerHTML = "<h2> Books and more books...</h2>";
+bookList2.innerHTML += "<p>Added on html</p>";
 
 
+const banner = document.querySelector("#page-banner");
+console.log('banner type is:', banner.nodeType);
+console.log('banner name is:', banner.nodeName);
+console.log('banner has childs:', banner.hasChildNodes);
+
+
+const cloneBanner = banner.cloneNode(true);
+console.log(cloneBanner);
+
+const bookList3 = document.querySelector("#book-list");
+console.log("The parent node: ", bookList3.parentNode);
+console.log("The parent node: ", bookList3.parentElement.parentElement);
+
+console.log(bookList3.childNodes);
