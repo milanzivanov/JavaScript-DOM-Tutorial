bf583e5b3f757ccc515b3263bfcd00bf998ebfef
milan.zivanov <milan.zivanov@gmail.com>
2018-09-06 11:43:22 +0200

    Traversing

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 
-const wmf = document.querySelector("#book-list li:nth-child(2) .name")
-// console.log(wmf);
-var books = document.querySelectorAll("#book-list li .name");
-// console.log(books);
+const bookList = document.querySelector("#book-list");
+console.log("The parent node: ", bookList.parentNode);
+console.log("The parent node: ", bookList.parentElement.parentElement);
+console.log(bookList.childNodes);
 
-books.forEach(book => {
-    book.textContent += ' (book title)';
-});
+console.log("book-list next sibling: ", bookList.nextSibling);
+console.log("book-list next sibling: ", bookList.nextElementSibling);
 
-const bookList2 = document.querySelector("#book-list");
-// bookList2.innerHTML = "<h2> Books and more books...</h2>";
-bookList2.innerHTML += "<p>Added on html</p>";
+console.log("book-list previous sibling is: ", bookList.previousSibling);
+console.log("book-list element sibling is: ", bookList.previousElementSibling);
 
 
-const banner = document.querySelector("#page-banner");
-console.log('banner type is:', banner.nodeType);
-console.log('banner name is:', banner.nodeName);
-console.log('banner has childs:', banner.hasChildNodes);
-
-
-const cloneBanner = banner.cloneNode(true);
-console.log(cloneBanner);
-
-const bookList3 = document.querySelector("#book-list");
-console.log("The parent node: ", bookList3.parentNode);
-console.log("The parent node: ", bookList3.parentElement.parentElement);
-
-console.log(bookList3.childNodes);
+bookList.previousElementSibling.querySelector("p").innerHTML += "<br /> Too cool for everyone else"
